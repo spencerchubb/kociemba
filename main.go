1e281f073b0ecf7f02b7432b4a842a3e4d694424
spencerchubb <spencerchubb@gmail.com>
2023-01-09 23:33:00 -0500

    error message

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package kociemba
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os/exec"
 )
@@ -9,7 +10,7 @@ func execCommand(command string, args ...string) (string, error) {
 	cmd := exec.Command(command, args...)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("execCommand error: %v: %v", stdout, err)
 	}
 	if err := cmd.Start(); err != nil {
 		return "", err
